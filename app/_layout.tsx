6e310ceb44fcd73e44e724a378afdb09e2b31da8
GabrielSheldes <gabriel.arantes9@etec.sp.gov.br>
2025-05-15 20:28:44 -0300

    initial commit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,49 @@
 import React from "react";
-import {Slot} from "expo-router";
+import {Slot, Tabs} from "expo-router";
+import { Ionicons } from '@expo/vector-icons';
+
 
 export default function Layout (){ 
-    return<Slot/>;
+    return (
+        <Tabs screenOptions={{tabBarActiveTintColor: 'blue'}}>
+            <Tabs.Screen 
+                name = 'index'
+                options={{
+                    title:'inicio',
+                    tabBarIcon: ({color, size }) => <Ionicons name ='home-outline' size = {size} color ={color}/>
+                }}
+                />
+                  <Tabs.Screen 
+                name = 'about'
+                options={{
+                    title:'Sobre',
+                    tabBarIcon: ({color, size }) => <Ionicons name ='information-outline' size = {size} color ={color}/>
+                }}
+                />
+                  <Tabs.Screen 
+                name = 'Settings'
+                options={{
+                    title:'Configurações',
+                    tabBarIcon: ({color, size }) => <Ionicons name ='settings-outline' size = {size} color ={color}/>
+                }}
+                />
+                  <Tabs.Screen 
+                name = 'credits'
+                options={{
+                    title:'Creditos',
+                    tabBarIcon: ({color, size }) => <Ionicons name ='credits-outline' size = {size} color ={color}/>
+                }}
+                />
+                
+                 <Tabs.Screen 
+                name = 'Files'
+                options={{
+                    title:'Pastas',
+                    tabBarIcon: ({color, size }) => <Ionicons name ='files-outline' size = {size} color ={color}/>
+                }}
+                />
+        </Tabs>
+    )
 }
   
 
